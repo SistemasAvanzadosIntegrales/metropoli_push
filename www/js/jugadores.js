605275c387af827370970f714c1f538e337ab144
Roberto <roberto_ramirez@avansys.com.mx>
2018-03-13 13:44:57 -0600

    first commit

--- www/js/jugadores.js
+++ www/js/jugadores.js
@@ -2,26 +2,6 @@
  * @author:   Andrea Luna
  * @contact:  andrea_luna@avansys.com.mx
  * @creation: 18/12/2017
- * @function: crear_nickname
- * @version:  1
- * @descripcion:
- **/
-/*
-function crear_nickname(jugador) {
-    
-	if( $("#nombre"+jugador).val() !== "" && 
-        $( "#apellido_paterno"+jugador ).val().length > 1 && 
-        $( "#nickname"+jugador ).val() === '' )
-    {
-        $( "#nickname"+jugador ).val( $("#nombre"+jugador).val().substr(0, 1) + $( "#apellido_paterno"+jugador ).val().substr(0,2));
-	}
-}
-*/
-
-/**
- * @author:   Andrea Luna
- * @contact:  andrea_luna@avansys.com.mx
- * @creation: 18/12/2017
  * @function: crearRegistro
  * @version:  1
  * @descripcion: Muestra el registro de clientes (nombre, apellido, correo).
@@ -31,9 +11,6 @@ function crearRegistro() {
 	var clientes = JSON.parse(localStorage.getItem('clientes'));
     var partida  = JSON.parse(localStorage.getItem('partida'));
 	var campo    = '';
-    
-    alert('Creando lista de jugadores...');
-    alert(clientes === null);
     
 	// Si existen clientes, muestra los registros ingresados anteriormente de caso contrario muestra los campos vacios
 	for(var i = 0, x = 1; i < partida['numeroPersonas']; i++, x++) {
@@ -45,12 +22,33 @@ function crearRegistro() {
 			campo += '<div class="col-12 mt-4"><img class="img-responsive margin_jugador" src="img/icono_registro_jugador.png" alt="icono_jugador" width="35px"><label class="col-12 texto enlinea ">Jugador '+x+'</label><div class="row justify-content-around mt-2"><input id="nombre'+x+'" name="nombre'+x+'" class="form-control" type="text" placeholder="Nombre" value="'+clientes[i]['nombre']+'" maxlength="100" readonly="readonly" /></div><div class="row justify-content-around mt-2"><input id="apellido_paterno'+x+'" name="apellido_paterno'+x+'" class="form-control col-6" type="text" placeholder="Paterno" value="'+clientes[i]['apellido_paterno']+'" maxlength="100" readonly="readonly" /><input id="apellido_materno'+x+'" name="apellido_materno'+x+'" class="form-control col-6" type="text" placeholder="Materno" value="'+clientes[i]['apellido_materno']+'" maxlength="100" readonly="readonly" /></div><div class="row justify-content-around mt-2"><input id="nickname'+x+'" name="nickname'+x+'" class="form-control col-4" type="text" placeholder="Nick" value="'+clientes[i]['nickname']+'" maxlength="4" readonly="readonly" /><input id="correo'+x+'" name="correo'+x+'" class="form-control col-8" type="email" placeholder="Correo" value="'+clientes[i]['correo']+'" maxlength="100" readonly="readonly" /></div></div>';
 		}
 	}
-	
-    alert(campo);
     
 	$("#jugadores").html(campo);
+}
+
+/**
+ * @author:   Andrea Luna
+ * @contact:  andrea_luna@avansys.com.mx
+ * @creation: 18/12/2017
+ * @function: crear_nickname
+ * @version:  1
+ * @descripcion:
+ **/
+function crear_nickname(jugador) {
     
-    alert('Lista de jugadores creada');
+    alert('Crear nickname');
+    
+	if( $("#nombre"+jugador).val() !== "" && 
+        $( "#apellido_paterno"+jugador ).val().length > 1 && 
+        $( "#nickname"+jugador ).val() === '' )
+    {
+        alert('entrando al if');
+        $( "#nickname"+jugador ).val( $("#nombre"+jugador).val().substr(0, 1) + $( "#apellido_paterno"+jugador ).val().substr(0,2));
+        alert('saliendo del if');
+	}
+    
+    alert('fin del nickname');
+    
 }
 
 /**
