31e4854ca334bd5ba9b28edb8c059a43e5d7b9f3
Roberto <roberto_ramirez@avansys.com.mx>
2018-03-13 12:58:49 -0600

    first commit

--- www/js/jugadores.js
+++ www/js/jugadores.js
@@ -8,9 +8,9 @@
  **/
 function crear_nickname(jugador) {
     
-	if( $("#nombre"+jugador).val() != "" && 
+	if( $("#nombre"+jugador).val() !== "" && 
         $( "#apellido_paterno"+jugador ).val().length > 1 && 
-        $( "#nickname"+jugador ).val() == '' )
+        $( "#nickname"+jugador ).val() === '' )
     {
         $( "#nickname"+jugador ).val( $("#nombre"+jugador).val().substr(0, 1) + $( "#apellido_paterno"+jugador ).val().substr(0,2));
 	}
@@ -30,17 +30,22 @@ function crearRegistro() {
     var partida  = JSON.parse(localStorage.getItem('partida'));
 	var campo    = '';
     
+    alert('Creando lista de jugadores...');
+    alert(clientes === null);
+    
 	// Si existen clientes, muestra los registros ingresados anteriormente de caso contrario muestra los campos vacios
 	for(var i = 0, x = 1; i < partida['numeroPersonas']; i++, x++) {
 		
-		if(clientes == null || clientes.length == 0){
-			campo += '<div class="col-12 mt-4"><img class="img-responsive margin_jugador" src="img/icono_registro_jugador.png" alt="icono_jugador" width="35px"><label class="col-12 texto enlinea ">Jugador '+x+'</label><div class="row justify-content-around mt-2"><input id="nombre'+x+'" name="nombre'+x+'" class="form-control" type="text" placeholder="Nombre" maxlength="100" onkeyup="crear_nickname('+x+')"></div><div class="row justify-content-around mt-2"><input id="apellido_paterno'+x+'" name="apellido_paterno'+x+'" class="form-control col-6" type="text" placeholder="Paterno" maxlength="100" onkeyup="crear_nickname('+x+')"><input id="apellido_materno'+x+'" name="apellido_materno'+x+'" class="form-control col-6" type="text" placeholder="Materno" maxlength="100"></div><div class="row justify-content-around mt-2"><input id="nickname'+x+'" name="nickname'+x+'" class="form-control col-4" type="text" placeholder="Nick" maxlength="3" /><input id="correo'+x+'" name="correo'+x+'" class="form-control col-8" type="email" placeholder="Correo" maxlength="100" /></div></div>'; 
+		if(clientes === null) {
+			campo += '<div class="col-12 mt-4"><img class="img-responsive margin_jugador" src="img/icono_registro_jugador.png" alt="icono_jugador" width="35px"><label class="col-12 texto enlinea ">Jugador '+x+'</label><div class="row justify-content-around mt-2"><input id="nombre'+x+'" name="nombre'+x+'" class="form-control" type="text" placeholder="Nombre" maxlength="100" onkeyup="crear_nickname('+x+')"></div><div class="row justify-content-around mt-2"><input id="apellido_paterno'+x+'" name="apellido_paterno'+x+'" class="form-control col-6" type="text" placeholder="Paterno" maxlength="100" onkeyup="crear_nickname('+x+')"><input id="apellido_materno'+x+'" name="apellido_materno'+x+'" class="form-control col-6" type="text" placeholder="Materno" maxlength="100"></div><div class="row justify-content-around mt-2"><input id="nickname'+x+'" name="nickname'+x+'" class="form-control col-4" type="text" placeholder="Nick" maxlength="3" /><input id="correo'+x+'" name="correo'+x+'" class="form-control col-8" type="email" placeholder="Correo" maxlength="100" /></div></div>';
 		}	
 		else {
 			campo += '<div class="col-12 mt-4"><img class="img-responsive margin_jugador" src="img/icono_registro_jugador.png" alt="icono_jugador" width="35px"><label class="col-12 texto enlinea ">Jugador '+x+'</label><div class="row justify-content-around mt-2"><input id="nombre'+x+'" name="nombre'+x+'" class="form-control" type="text" placeholder="Nombre" value="'+clientes[i]['nombre']+'" maxlength="100" readonly="readonly" /></div><div class="row justify-content-around mt-2"><input id="apellido_paterno'+x+'" name="apellido_paterno'+x+'" class="form-control col-6" type="text" placeholder="Paterno" value="'+clientes[i]['apellido_paterno']+'" maxlength="100" readonly="readonly" /><input id="apellido_materno'+x+'" name="apellido_materno'+x+'" class="form-control col-6" type="text" placeholder="Materno" value="'+clientes[i]['apellido_materno']+'" maxlength="100" readonly="readonly" /></div><div class="row justify-content-around mt-2"><input id="nickname'+x+'" name="nickname'+x+'" class="form-control col-4" type="text" placeholder="Nick" value="'+clientes[i]['nickname']+'" maxlength="4" readonly="readonly" /><input id="correo'+x+'" name="correo'+x+'" class="form-control col-8" type="email" placeholder="Correo" value="'+clientes[i]['correo']+'" maxlength="100" readonly="readonly" /></div></div>';
 		}
 	}
 	
+    alert(campo);
+    
 	$("#jugadores").html(campo);
     
     alert('Lista de jugadores creada');
@@ -64,7 +69,7 @@ function iniciarJuego() {
 	
 	var registro_jugadores = new Array();
 	var vacio = false;
-    var correo = false;
+    //var correo = false;
 	
 	//Ciclo para revisar que no haya campos vacios y llenar el arreglo con los nombres ingresdos para enviarlo a guardar.
 	for(var i = 1; i < parseInt(partida['numeroPersonas']) + 1; i++) {
